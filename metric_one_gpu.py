09228c0e644358f68c2e1bde1139dd6a6ad98483
KingJames <3553416402@qq.com>
2025-04-13 23:18:08 +0800

    modify eval

--- metric_one_gpu.py
+++ metric_one_gpu.py
@@ -43,13 +43,13 @@ os.environ["HTTP_PROXY"] = "http://127.0.0.1:7890"
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 psnr_metric = pyiqa.create_metric('psnr', device=device)
 ssim_metric = pyiqa.create_metric('ssim', device=device)
-fid_metric = pyiqa.create_metric('fid', device=device)
-maniqa_metric = pyiqa.create_metric('maniqa', device=device)
-lpips_iqa_metric = pyiqa.create_metric('lpips', device=device)
-clipiqa_iqa_metric = pyiqa.create_metric('clipiqa', device=device)
-musiq_iqa_metric = pyiqa.create_metric('musiq', device=device)
-dists_iqa_metric = pyiqa.create_metric('dists', device=device)
-niqe_iqa_metric = pyiqa.create_metric('niqe', device=device)
+# fid_metric = pyiqa.create_metric('fid', device=device)
+# maniqa_metric = pyiqa.create_metric('maniqa', device=device)
+# lpips_iqa_metric = pyiqa.create_metric('lpips', device=device)
+# clipiqa_iqa_metric = pyiqa.create_metric('clipiqa', device=device)
+# musiq_iqa_metric = pyiqa.create_metric('musiq', device=device)
+# dists_iqa_metric = pyiqa.create_metric('dists', device=device)
+# niqe_iqa_metric = pyiqa.create_metric('niqe', device=device)
 
 def normalize_01_into_pm1(x):  # normalize x from [0, 1] to [-1, 1] by (x*2) - 1
     return x.add(x).add_(-1)
@@ -114,6 +114,7 @@ def get_img(args, ld_val, maxtot, ckpt_paths):
         out_dir = os.path.join("metric_results",os.path.basename(vae_ckpt))
         predict_dir = os.path.join(out_dir,"predict")
         gt_dir = os.path.join(out_dir,"gt")
+        vaex_first_rec_dir = os.path.join(out_dir,"vaex_first_rec")
         
         if os.path.exists(out_dir):
             print(f"{vae_ckpt} exist, skip!!!")
@@ -121,6 +122,7 @@ def get_img(args, ld_val, maxtot, ckpt_paths):
         
         os.makedirs(predict_dir,exist_ok=True)
         os.makedirs(gt_dir,exist_ok=True)
+        os.makedirs(vaex_first_rec_dir,exist_ok=True)
         
         for index, data in tqdm(enumerate(ld_val), total=len(ld_val)):
             if maxtot < index and maxtot > 0:
@@ -129,16 +131,25 @@ def get_img(args, ld_val, maxtot, ckpt_paths):
                 # data #[-1,1]
                 data = data.to(args.device)
                 rec_B3HW, usage , Lq  = vae(data) 
+
+                vaex_first_rec = vae.decoder(vae.post_quant_conv(vae.quant_conv(vae.encoder(data))))
+
                 rec_B3HW = rec_B3HW.clamp(-1, 1)
+                vaex_first_rec = vaex_first_rec.clamp(-1, 1)
                 data =  (data.cpu().squeeze(0).numpy() + 1.0 ) * 255.0 / 2
                 rec_B3HW =  (rec_B3HW.cpu().squeeze(0).numpy() + 1.0 ) * 255.0 / 2
+                vaex_first_rec =  (vaex_first_rec.cpu().squeeze(0).numpy() + 1.0 ) * 255.0 / 2
                 data = data.astype(np.uint8)
                 rec_B3HW = rec_B3HW.astype(np.uint8)
+                vaex_first_rec = vaex_first_rec.astype(np.uint8)
                 for i in range(data.shape[0]):
                     _data = data[i].transpose(1,2,0)
                     _rec_B3HW = rec_B3HW[i].transpose(1,2,0)
+                    _vaex_first_rec = vaex_first_rec[i].transpose(1,2,0)
                     Image.fromarray(_rec_B3HW).save(os.path.join(predict_dir,f"{index*data.shape[0]+i}.png"))
                     Image.fromarray(_data).save(os.path.join(gt_dir,f"{index*data.shape[0]+i}.png"))
+                    Image.fromarray(_vaex_first_rec).save(os.path.join(vaex_first_rec_dir,f"{index*data.shape[0]+i}.png"))
+
 
 def metric(metric_path,ckpt_paths):
 
@@ -153,11 +164,14 @@ def metric(metric_path,ckpt_paths):
         out_dir = os.path.join("metric_results",fold)
         predict_dir = os.path.join(out_dir,"predict")
         gt_dir = os.path.join(out_dir,"gt")
+        vaex_first_rec_dir = os.path.join(out_dir,"vaex_first_rec")
         
         gt_img_paths = []
 
         psnr_folder = []
         ssim_folder = []
+        psnr_first_rec = []
+        ssim_first_rec = []
         lpips_score = []
         dists_score = []
         niqe_score = []
@@ -171,36 +185,44 @@ def metric(metric_path,ckpt_paths):
         for gt_img_path in tqdm(gt_img_paths):
             GT_image = img_preproc(Image.open(gt_img_path).convert('RGB'))
             prediction_img_path = gt_img_path.replace("/gt/", "/predict/")
-            VARPrediction_img = img_preproc(Image.open(prediction_img_path).convert('RGB'))
-        
+            vaex_first_rec_path = gt_img_path.replace("/gt/", "/vaex_first_rec/")
+
             img1 = rgb2ycbcr_pt(img2tensor(io.imread(gt_img_path)),  y_only=True).to(torch.float64)
             img2 = rgb2ycbcr_pt(img2tensor(io.imread(prediction_img_path)),  y_only=True).to(torch.float64)
+            img3 = rgb2ycbcr_pt(img2tensor(io.imread(vaex_first_rec_path)),  y_only=True).to(torch.float64)
             img1 = torch.squeeze(img1)
             img2 = torch.squeeze(img2)
-            
+            img3 = torch.squeeze(img3)
+
             ssim_folder.append(ssim_metric(img1.unsqueeze(0).unsqueeze(0), img2.unsqueeze(0).unsqueeze(0)))
             psnr_folder.append(psnr_metric(img1.unsqueeze(0).unsqueeze(0), img2.unsqueeze(0).unsqueeze(0)))
-            lpips_iqa.append(lpips_iqa_metric(prediction_img_path, gt_img_path))
-            clip_iqa.append(clipiqa_iqa_metric(prediction_img_path))
-            musiq_iqa.append(musiq_iqa_metric(prediction_img_path))
-            maniqa_iqa.append(maniqa_metric(prediction_img_path))
-            dists_score.append(dists_iqa_metric(prediction_img_path, gt_img_path))
-            niqe_score.append(niqe_iqa_metric(prediction_img_path))
+
+            ssim_first_rec.append(ssim_metric(img1.unsqueeze(0).unsqueeze(0), img3.unsqueeze(0).unsqueeze(0)))
+            psnr_first_rec.append(psnr_metric(img1.unsqueeze(0).unsqueeze(0), img3.unsqueeze(0).unsqueeze(0)))
+            # lpips_iqa.append(lpips_iqa_metric(prediction_img_path, gt_img_path))
+            # clip_iqa.append(clipiqa_iqa_metric(prediction_img_path))
+            # musiq_iqa.append(musiq_iqa_metric(prediction_img_path))
+            # maniqa_iqa.append(maniqa_metric(prediction_img_path))
+            # dists_score.append(dists_iqa_metric(prediction_img_path, gt_img_path))
+            # niqe_score.append(niqe_iqa_metric(prediction_img_path))
 
         with open(metric_path, "a") as f:
             f.write(f"fold = {fold}\n")
         write_metrics_to_file(metric_path, "PSNR", psnr_folder)
         write_metrics_to_file(metric_path, "SSIM", ssim_folder)
-        write_metrics_to_file(metric_path, "LPIPS", lpips_iqa)
-        write_metrics_to_file(metric_path, "DISTS", dists_score)
-        write_metrics_to_file(metric_path, "NIQE", niqe_score)
-        write_metrics_to_file(metric_path, "CLIP-IQA", clip_iqa)
-        write_metrics_to_file(metric_path, "MUSIQ", musiq_iqa)
-        write_metrics_to_file(metric_path, "MANIQA", maniqa_iqa)
-        print(f"now fid")
-        fid_value = fid_metric(gt_dir, predict_dir)
-        with open(metric_path, "a") as f:
-            f.write(f"FID = {get_value(fid_value)}\n")
+
+        write_metrics_to_file(metric_path, "PSNR_first_rec", psnr_first_rec)
+        write_metrics_to_file(metric_path, "SSIM_first_rec", ssim_first_rec)
+        # write_metrics_to_file(metric_path, "LPIPS", lpips_iqa)
+        # write_metrics_to_file(metric_path, "DISTS", dists_score)
+        # write_metrics_to_file(metric_path, "NIQE", niqe_score)
+        # write_metrics_to_file(metric_path, "CLIP-IQA", clip_iqa)
+        # write_metrics_to_file(metric_path, "MUSIQ", musiq_iqa)
+        # write_metrics_to_file(metric_path, "MANIQA", maniqa_iqa)
+        # print(f"now fid")
+        # fid_value = fid_metric(gt_dir, predict_dir)
+        # with open(metric_path, "a") as f:
+        #     f.write(f"FID = {get_value(fid_value)}\n")
         
 
 if __name__ == "__main__":
@@ -231,5 +253,5 @@ if __name__ == "__main__":
 
     setup(0,1)
     
-    # get_img(args,ld_val,maxtot,ckpt_paths)
+    get_img(args,ld_val,maxtot,ckpt_paths)
     metric(out_path,ckpt_paths)
